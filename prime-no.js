3de33c373b2ebe32292df20934bdfd5aa671cd6e
unknown <shreyanksupare140499@gmail.com>
2022-12-10 10:03:59 +0530

    checkit

---
+++ prime-no.js
@@ -0,0 +1,15 @@
+ var num=13
+ let count=0;
+    for(let i=0;i<=num;i++)
+{
+        if(num%i===0)
+        count++
+    }
+if(count===1)
+{
+        console.log("Yes")
+    }
+else
+{
+        console.log("No")
+    }
